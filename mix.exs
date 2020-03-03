6e68b530968d3918f6219f7ffd1686a89af84315
Edison Yap <edisonywh@gmail.com>
2020-03-03 21:46:49 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -40,12 +40,12 @@ defmodule Readability.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:floki, "~> 0.21.0"},
+      {:floki, "~> 0.26.0"},
       {:httpoison, "~> 1.5"},
       {:ex_doc, "~> 0.14", only: :dev},
-      {:credo, "~> 0.9", only: [:dev, :test]},
+      {:credo, "~> 1.2.3", only: [:dev, :test]},
       {:dialyxir, "~> 0.3", only: [:dev]},
-      {:mock, "~> 0.2.0", only: :test}
+      {:mock, "~> 0.3", only: :test}
     ]
   end
 
